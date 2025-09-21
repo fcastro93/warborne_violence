b2f3d23cc60318d068ad13f446394ceda1c9bb9b
Fernando Castro Mendez <fcastro.mendez.work@gmail.com>
2025-09-20 23:34:44 -0600

    Fix settings default to production: manage.py, wsgi.py, asgi.py, buildpack scripts

--- warborne_tools/wsgi.py
+++ warborne_tools/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'warborne_tools.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'warborne_tools.settings_production')
 
 application = get_wsgi_application()
