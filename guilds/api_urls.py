133b7b203af45512fed9c5b7de00103774a44faf
Fernando Castro Mendez <fcastro.mendez.work@gmail.com>
2025-09-23 04:50:02 -0600

    Events

--- guilds/api_urls.py
+++ guilds/api_urls.py
@@ -4,7 +4,7 @@ from . import api_views
 urlpatterns = [
     path('stats/', api_views.guild_stats, name='guild_stats'),
     path('members/', api_views.guild_members, name='guild_members'),
-    path('events/', api_views.recent_events, name='recent_events'),
+    path('recent-events/', api_views.recent_events, name='recent_events'),
     path('gear/', api_views.gear_overview, name='gear_overview'),
     path('gear-items/', api_views.gear_items, name='gear_items'),
     path('builds/', api_views.recommended_builds, name='recommended_builds'),
