2b1e2c662b8b3355a05cacacc83738fa9b9dd6fa
Fernando Castro Mendez <fcastro.mendez.work@gmail.com>
2025-09-27 20:14:12 -0600

    Fix S3 region mismatch for violenceguild bucket
    - Update AWS_S3_REGION_NAME default from us-east-1 to us-east-2
    - Fixes PermanentRedirect error when accessing S3 bucket
    - Violenceguild bucket is located in us-east-2 region
    - Updates settings.py, settings_dev.py, and settings_ec2.py

--- warborne_tools/settings.py
+++ warborne_tools/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-2')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
